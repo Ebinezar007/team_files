37289af1950d2016c9b4af68545ce9f93a69f6ea
Ebinezar <56664148+Ebinezar@users.noreply.github.com>
2021-01-30 16:25:39 +0530

    Add All Files

---
+++ app-secure/aslide.php
@@ -0,0 +1,67 @@
+<div class="sidebar">
+            <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red"
+    -->
+            <div class="sidebar-wrapper">
+                <div class="logo">
+                    <a href="javascript:void(0)" class="simple-text logo-mini">
+                        <img src="https://ik.imagekit.io/bfzb9z4tav/assets/favicon_FUJbfCUAh.ico">
+                    </a>
+                    <a href="javascript:void(0)" class="simple-text logo-normal">
+           NUTZ
+          </a>
+                </div>
+                <ul class="nav">
+                <y url="/index">
+                    <li class="active">
+                      <a href="#">
+                            <i class="tim-icons icon-chart-pie-36"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li></y>
+                    <li>
+                        <a data-toggle="collapse" href="#pagesExamples">
+                            <i class="tim-icons icon-image-02"></i>
+                            <p>
+                                Pages
+                                <b class="caret"></b>
+                            </p>
+                        </a>
+                        <div class="collapse" id="pagesExamples">
+                            <ul class="nav">
+                               
+                            <y url="/timeline"><li>
+                                    <a href="#">
+                                        <span class="sidebar-mini-icon">T</span>
+                                        <span class="sidebar-normal"> Timeline </span>
+                                    </a>
+                                   </li></y>
+                                    <y url="/timeline1"><li>
+                                    <a href="#">
+                                        <span class="sidebar-mini-icon">T</span>
+                                        <span class="sidebar-normal"> Timeline2 </span>
+                                    </a>
+                                    </li></y>
+                                
+                                    <y url="/user"><li>
+                                    <a href="#">
+                                        <span class="sidebar-mini-icon">UP</span>
+                                        <span class="sidebar-normal"> User Profile </span>
+                                    </a>
+                                </li></y>
+                            </ul>
+                        </div>
+                    </li>
+                    <y url="/employeManagement"><li>
+                        <a href="#">
+                            <i class="tim-icons icon-single-02"></i>
+                            <p>
+                                EMPLOYEE
+                               
+                            </p>
+                        </a>
+                    </li></y>
+                   
+                </ul>
+            </div>
+        </div>
